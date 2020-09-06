d00ed59c6f4a164428a5a10390351d3ec4e68fec
IT Vision Hub <info.ivhub@gmail.com>
2020-09-06 11:31:26 +0630

    Adding Template

--- app/views/components/footer.php
+++ app/views/components/footer.php
@@ -1,3 +1,7 @@
+
+<script src="<?php echo URLROOT; ?>/public/js/jquery.min.js"></script>
+<script src="<?php echo URLROOT; ?>/public/js/popper.js"></script>
+<script src="<?php echo URLROOT; ?>/public/js/bootstrap.min.js"></script>
 <script src="<?php echo URLROOT; ?>/js/main.js"></script>
 </body>
 
--- app/views/components/header.php
+++ app/views/components/header.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
     <title><?php echo SITENAME; ?></title>
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="<?php echo URLROOT; ?>/public/css/style.css">
 </head>
 
 <body>
