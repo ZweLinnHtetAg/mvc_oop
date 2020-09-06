d806e5f74d9845dbaec2330e3a52efa49a6b4d6d
IT Vision Hub <info.ivhub@gmail.com>
2020-09-06 09:41:15 +0630

    initial project setup

---
+++ app/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+// Database connection info
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'project');
+define('DB_CHARSET', 'utf8');
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL Root
+define('URLROOT', 'http://localhost/project');
+
+// Site Name
+define('SITENAME', 'ITvisionhub');
